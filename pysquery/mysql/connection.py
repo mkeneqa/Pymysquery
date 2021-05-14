68bde018e86d4504496ac00bbb07c3968cecf70b
mike.keneqa <mike.keneqa@onstream-pipeline.com>
2021-05-14 13:38:59 -0600

    WIP: mysql connection logic

--- pysquery/mysql/connection.py
+++ pysquery/mysql/connection.py
@@ -5,7 +5,7 @@ from prodict import Prodict
 from ..connection_base import ConnectionBase
 
 
-class Config(Prodict):
+class _Config(Prodict):
     host: str
     port: int
     db_name: str
@@ -21,26 +21,56 @@ class Config(Prodict):
     def enable_dict_cursor(self):
         self.cursor_class = pymysql.cursors.DictCursor
 
+    def is_ready(self):
+        skip_cols = ['charset', 'cursor_class']
+        for k, v in self.items():
+            if k not in skip_cols:
+                if v is None:
+                    return False
+
+        return True
+
 
 class Connection(ConnectionBase):
     user_cursor_dictionary = False
     CONN = None
+    config = _Config()
+
+    def __init__(self, host='', port='', db_name='', db_user='', db_passwd='', charset='utf8mb4', enable_cursor=False):
+        if host:
+            self.config.host = host
+        if port:
+            self.config.port = port
+        if db_name:
+            self.config.db_name = db_name
+        if db_user:
+            self.config.db_user = db_user
+        if db_passwd:
+            self.config.passwd = db_passwd
+
+        self.config.charset = charset
+        if enable_cursor:
+            self.config.enable_dict_cursor()
 
     def connect(self):
         # TODO: implement config
-        config = Config()
-        if self.user_cursor_dictionary:
+        if self.config.is_ready():
             self.CONN = pymysql.connect(
-                host=config.host,
-                port=config.port,
-                user=config.db_user,
-                password=config.db_passwd,
-                db=config.db_name,
-                charset=config.charset
+                host=self.config.host,
+                port=self.config.port,
+                user=self.config.db_user,
+                password=self.config.db_passwd,
+                db=self.config.db_name,
+                charset=self.config.charset
             )
 
-            if config.cursor_class is not None:
-                self.CONN.cursor(config.cursor_class)
+            if self.config.cursor_class is not None:
+                self.CONN.cursor(self.config.cursor_class)
+
+            return self.CONN
+        else:
+            # TODO: Throw exception
+            pass
 
     def connection_handler(self):
         pass
